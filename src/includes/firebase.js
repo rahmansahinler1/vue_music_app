a4ec515fd85257133c7db8ec93209aba1d2148ef
Rahman Sahinler <rahmansahinler1@gmail.com>
2025-09-13 14:43:32 +0300

    fix: keys

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -3,11 +3,11 @@ import 'firebase/auth'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAHBPjf3n__nubTObFjn7tw_i8efbkCh4c',
-  authDomain: 'musicapp-86a14.firebaseapp.com',
-  projectId: 'musicapp-86a14',
-  storageBucket: 'musicapp-86a14.firebasestorage.app',
-  appId: '1:518054012707:web:848b70f8b87495b37b8e96',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 }
 
 export default firebase.initializeApp(firebaseConfig)
